0ed101e7c44d7fd6a19ee2c3190a2a326257161b
GUSSCH <90836568+GUSSCHO74@users.noreply.github.com>
2022-11-07 11:03:18 +0100

    Update skript.php

--- skript.php
+++ skript.php
@@ -1,31 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "webbserverprogrammering";
-$conn = new mysqli($servername, $username, $password, $dbname);
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "webbserverprogrammering";
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+	$sql = "SELECT * FROM users";
+	$result = $conn->query($sql);	
 
-$sql = "SELECT * FROM users";
-$result = $conn->query($sql);
-
-$login_success = false;
-$full_name = "";
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-		if($row["userId"] == $_POST["username"] &&
-					$row["passwd"] == $_POST["password"]) {
-			$login_success = true;
-			$full_name = $row["firstname"] . " " .
-					$row["lastname"];
+	$login_success = false;
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			if($row["username"] == $_POST["username"] && $row["password"] == $_POST["password"]){
+				$login_success = true;
+				echo "<h1>Welcome!</h1>";
 			}
-	}
-} else {
-    echo "0 results";
-}
-$conn->close();
+			else {
+				echo "<h1>Login failed!</h1><p>Wrong username and password combination!</p>";	
+			}
+		}
+	} 
+	$conn->close();
 ?>
+</body>
+</html>
